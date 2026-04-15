de11e0c933267c7d838f820a027bdcee44f9a4ef
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-04-15 23:59:45 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -42,29 +42,41 @@ async def channel_post(client: Client, message: Message):
         return
 
     reply_text = await message.reply_text("Please Wait...! 🫷", quote=True)
-    try:
-        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
-    except Exception as e:
-        print(e)
+
+    db_channels = getattr(client, 'db_channels', [client.db_channel])
+    links = []
+
+    for db_ch in db_channels:
+        try:
+            post_message = await message.copy(chat_id=db_ch.id, disable_notification=True)
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            post_message = await message.copy(chat_id=db_ch.id, disable_notification=True)
+        except Exception as e:
+            print(e)
+            continue
+
+        fs_param = "fs_" + base64.b64encode(str(post_message.id).encode()).decode()
+        link = f"https://t.me/{client.username}?start={fs_param}"
+        links.append((db_ch.title or str(db_ch.id), link))
+
+        if not DISABLE_CHANNEL_BUTTON:
+            reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Get File", url=link)]])
+            try:
+                await post_message.edit_reply_markup(reply_markup)
+            except FloodWait as e:
+                await asyncio.sleep(e.value)
+                await post_message.edit_reply_markup(reply_markup)
+            except Exception:
+                pass
+
+    if not links:
         await reply_text.edit_text("Something went Wrong..!")
         return
 
-    fs_param = "fs_" + base64.b64encode(str(post_message.id).encode()).decode()
-    link = f"https://t.me/{client.username}?start={fs_param}"
-
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Get File", url=link)]])
-    await reply_text.edit(f"<b>Here is your link :</b>\n{link}", reply_markup=reply_markup, disable_web_page_preview=True)
-    if not DISABLE_CHANNEL_BUTTON:
-        try:
-            await post_message.edit_reply_markup(reply_markup)
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await post_message.edit_reply_markup(reply_markup)
-        except Exception:
-            pass
+    buttons = [[InlineKeyboardButton(f"📁 {title}", url=lnk)] for title, lnk in links]
+    text = "<b>Here are your links:</b>\n\n" + "\n".join(f"• <a href='{lnk}'>{title}</a>" for title, lnk in links)
+    await reply_text.edit(text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
 
 @Bot.on_message(filters.channel & filters.incoming & dynamic_channel_filter)
